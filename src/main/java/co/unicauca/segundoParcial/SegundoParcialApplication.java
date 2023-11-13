2c07d01aa3be180173c9365ba7eb8d4a9b926434
DanielSolarte3 <danielsolarte1016@gmail.com>
2023-11-13 15:20:44 -0500

    Creacion Proyecto

---
+++ src/main/java/co/unicauca/segundoParcial/SegundoParcialApplication.java
@@ -0,0 +1,13 @@
+package co.unicauca.segundoParcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoParcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/unicauca/segundoParcial/SegundoParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.segundoParcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
