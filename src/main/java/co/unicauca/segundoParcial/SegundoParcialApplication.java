17dbc887909adee774f84f68f6d5c6a823b81034
CrisQR <cdqr27@gmail.com>
2023-11-13 18:29:48 -0500

    Implementación de swagger para realizar la documentación del proyecto

--- src/main/java/co/unicauca/segundoParcial/SegundoParcialApplication.java
+++ src/main/java/co/unicauca/segundoParcial/SegundoParcialApplication.java
@@ -1,13 +1,24 @@
 package co.unicauca.segundoParcial;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SegundoParcialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SegundoParcialApplication.class, args);
+	}
+
+
+	@Bean
+	public OpenAPI custonOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("SOFTWARE DE SEGUIMIENTO DE ACCIONES")
+				.version("1.0.0").description("Software que permite realizar un seguimiento de las acciones a los usuarios en una aplicación con Spring Boot."));
 	}
 
 }
