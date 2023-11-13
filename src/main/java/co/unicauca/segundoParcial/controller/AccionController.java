bd084d79142062655528c083bc2675fd0b5bb832
Joan Sebastian Tuquerrez Gomez <jtuquerrez@unicauca.edu.co>
2023-11-13 17:31:09 -0500

    Accion controller modified

--- src/main/java/co/unicauca/segundoParcial/controller/AccionController.java
+++ src/main/java/co/unicauca/segundoParcial/controller/AccionController.java
@@ -1,7 +1,26 @@
 package co.unicauca.segundoParcial.controller;
 
+import co.unicauca.segundoParcial.access.BolsaValoresRepository;
+import co.unicauca.segundoParcial.model.Accion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/accion")
 public class AccionController {
+    @Autowired
+    private DataSource dataSource;
 
+    @Autowired
+    private BolsaValoresRepository bolsaValoresRepository;
+
+    @GetMapping("/list")
+    public List<Accion> list() {
+        return bolsaValoresRepository.findAllActions();
+    }
 }
