712b20212dfa0e5b3fff7d40c7621ca0658f016c
Joan Sebastian Tuquerrez Gomez <jtuquerrez@unicauca.edu.co>
2023-11-13 22:30:41 -0500

    UsuarioController implemented

--- src/main/java/co/unicauca/segundoParcial/controller/NotificacionController.java
+++
@@ -1,4 +0,0 @@
-package co.unicauca.segundoParcial.controller;
-
-public class NotificacionController {
-}
--- src/main/java/co/unicauca/segundoParcial/controller/UsuarioController.java
+++ src/main/java/co/unicauca/segundoParcial/controller/UsuarioController.java
@@ -1,7 +1,10 @@
 package co.unicauca.segundoParcial.controller;
 
 import co.unicauca.segundoParcial.access.SqlLiteRepository;
+import co.unicauca.segundoParcial.model.Usuario;
+import io.micrometer.common.lang.NonNull;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/usuario")
 public class UsuarioController {
     private SqlLiteRepository sqlLiteRepository = new SqlLiteRepository();
+
+    @PostMapping
+    public void createUsuario(@RequestBody @NonNull Usuario user){
+        sqlLiteRepository.saveUser(user);
+    }
 }
